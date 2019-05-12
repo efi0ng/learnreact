30214480edd5c76130aa53b3f217d81b743de9f5
efi0ng <git@efi0ng.com>
2019-05-12 17:42:22 +0100

    returning to previous state before form section

--- li_resst/firstapp/src/index.js
+++ li_resst/firstapp/src/index.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-class FaveColourForm extends React.Component {
-    state = { value: '' }
-
-    newColor = e => this.setState({value: e.target.value });
-
-    submit = e => {
-        console.log(`New colour: ${this.state.value}`);
-        e.preventDefault();
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.submit}>
-                <label>Favourite Colour:
-                    <input type="color" onChange={this.newColor}/>
-                </label>
-                <button>Submit</button>
-            </form>
-        );
-    }
-}
+import { Library, bookList } from './Library';
 
 render(
-    <FaveColourForm />,
+    <Library books={bookList} visitorStep={1} />,
     document.getElementById('root')
 )
