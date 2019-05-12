13a1177df8835eca17d1fb6704998c1cdf94f989
efi0ng <git@efi0ng.com>
2019-05-12 14:03:57 +0100

    exploring forms.

--- li_resst/firstapp/src/index.js
+++ li_resst/firstapp/src/index.js
@@ -1,8 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import {Library, bookList} from './Library';
+
+class FaveColourForm extends React.Component {
+    state = { value: '' }
+
+    newColor = e => this.setState({value: e.target.value });
+
+    submit = e => {
+        console.log(`New colour: ${this.state.value}`);
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submit}>
+                <label>Favourite Colour:
+                    <input type="color" onChange={this.newColor}/>
+                </label>
+                <button>Submit</button>
+            </form>
+        );
+    }
+}
 
 render(
-    <Library books={bookList} visitorStep={2} />,
+    <FaveColourForm />,
     document.getElementById('root')
 )
